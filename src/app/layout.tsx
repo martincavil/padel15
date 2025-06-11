fd50e5c7eced040ec0251d189150e8ed1c60efa0
martincavil <martincavil.dev@gmail.com>
2025-06-11 18:30:20 +0200

    add new favicon and update layout to use the new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="fr">
       <head>
       <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
-      <link rel="icon" href="chemin/vers/votre-favicon.ico" type="image/x-icon"></link>
+      <link rel="icon" href='/P15.ico' type="image/x-icon"></link>
       </head>
       <body className="scroll-smooth font-sans">
         <Layout>{children}</Layout>
